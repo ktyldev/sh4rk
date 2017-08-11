b2f2ffe51ea250265f7f1265c61e7bc2beabda61
Cat Flynn <mewtwo97@hotmail.com>
2017-08-11 21:00:18 +0100

    Initial commit

---
+++ Assets/TwinStickShark/Shark/Shark.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shark : MonoBehaviour {
+
+    public float speed;
+
+    private Vector3 _direction;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+    }
+
+    private void Move() {
+        _direction = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal")).normalized;
+
+        if (_direction == Vector3.zero)
+            return;
+
+        transform.LookAt(transform.position + _direction);
+
+        var moveDir = new Vector3(_direction.z, 0, -_direction.x);
+        transform.Translate(moveDir * Time.deltaTime * speed, Space.World);
+    }
+}
