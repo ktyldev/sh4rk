29b38437c215b58a7226fb03c5ece7e929f76894
Cat Flynn <mewtwo97@hotmail.com>
2017-08-11 22:06:47 +0100

    Spawn shark

---
+++ Assets/TwinStickShark/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject shark;
+
+    private Shark _shark;
+
+	// Use this for initialization
+	void Start () {
+        _shark = Instantiate(shark).GetComponent<Shark>();
+        Camera.main.GetComponent<CameraController>().trackedObject = _shark.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
