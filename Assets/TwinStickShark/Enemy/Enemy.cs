9004679541199d7c89ff963943f288124d202510
Cat Flynn <mewtwo97@hotmail.com>
2017-08-12 13:48:34 +0100

    Add enemy

---
+++ Assets/TwinStickShark/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10;
+
+    private Transform _player;
+
+	// Use this for initialization
+	void Start () {
+        _player = Player.instance.sharkTransform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(_player);
+
+        var dir = _player.transform.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+	}
+}
--- Assets/TwinStickShark/Player/Player.cs
+++ Assets/TwinStickShark/Player/Player.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public static Player instance { get; private set; }
+
     public GameObject shark;
 
     private Shark _shark;
 
-	// Use this for initialization
-	void Start () {
+    public Transform sharkTransform { get { return _shark.transform; } }
+
+    void Awake() {
+        if (instance != null)
+            throw new System.Exception();
+
+        instance = this;
         _shark = Instantiate(shark).GetComponent<Shark>();
+    }
+
+    // Use this for initialization
+    void Start() {
         Camera.main.GetComponent<CameraController>().trackedObject = _shark.transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 }
