3b6cf4a1f5880489a93f69521d5383866d6774a5
Cat Flynn <mewtwo97@hotmail.com>
2017-08-12 15:19:47 +0100

    Add score display

---
+++ Assets/TwinStickShark/UI/PlayerScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour {
+
+    private Player _player;
+    private Text _text;
+
+    void Start() {
+        _player = Player.instance;
+        _text = GetComponent<Text>();
+    }
+
+    void OnGUI() {
+        _text.text = string.Format("Score: {0}", _player.score);
+    }
+}
