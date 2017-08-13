a91cc5d0f37447552c964cc4242edb86012a483d
Cat Flynn <mewtwo97@hotmail.com>
2017-08-13 18:18:58 +0100

    Add keyboard movement

---
+++ Assets/TwinStickShark/Input/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    private static InputManager _instance;
+
+    public ControlMode controlMode;
+
+    private static Vector3 _direction;
+
+    void Awake() {
+        if (_instance != null) 
+            throw new System.Exception();
+
+        _instance = this;
+        _direction = new Vector3();
+    }
+
+    public static Vector3 GetMoveDirection() {
+        switch (_instance.controlMode) {
+            case ControlMode.MouseKeyboard:
+                _direction = Vector3.zero;
+
+                if (Input.GetKey(KeyCode.A)) {
+                    _direction.x = -1;
+                }
+                if (Input.GetKey(KeyCode.D)) {
+                    _direction.x = 1;
+                }
+                if (Input.GetKey(KeyCode.W)) {
+                    _direction.z = 1;
+                }
+                if (Input.GetKey(KeyCode.S)) {
+                    _direction.z = -1;
+                }
+
+                break;
+            case ControlMode.Gamepad:
+                _direction.x = Input.GetAxis("Horizontal");
+                _direction.z = Input.GetAxis("Vertical");
+                break;
+
+            default:
+                break;
+        }
+
+        return _direction;
+    }
+}
--- Assets/TwinStickShark/Shark/Shark.cs
+++ Assets/TwinStickShark/Shark/Shark.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shark : MonoBehaviour {
-
+    
     public GameObject mount;
     
-    private Vector3 _direction;
     private Mover _mover;
 
     void Awake() {
-        _direction = new Vector3();
         _mover = GetComponent<Mover>();
     }
     
@@ -23,9 +21,6 @@ public class Shark : MonoBehaviour {
     }
 
     private void Move() {
-        _direction.x = Input.GetAxis("Horizontal");
-        _direction.z = Input.GetAxis("Vertical");
-        
-        _mover.SetDirection(_direction);
+        _mover.SetDirection(InputManager.GetMoveDirection());
     }
 }
