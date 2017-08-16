24c779cef4971c85fe80dc55f318262f2703e5b1
Cat Flynn <mewtwo97@hotmail.com>
2017-08-16 22:34:05 +0100

    Add boost powerup

---
+++ Assets/SH4RK/Powerup/Boost.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : MonoBehaviour {
+
+    public float duration;
+    public float speedMultiplier;
+
+    private Mover _mover;
+    private float _originalSpeed;
+
+    void Awake() {
+        _mover = GetComponentInParent<Mover>();
+        if (_mover == null)
+            throw new System.Exception();
+
+
+        _originalSpeed = _mover.speed;
+        _mover.speed *= speedMultiplier;
+        Destroy(gameObject, duration);
+    }
+
+    void OnDestroy() {
+        _mover.speed = _originalSpeed;    
+    }
+}
---
+++ Assets/SH4RK/Powerup/BoostPowerup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostPowerup : Powerup {
+
+    public GameObject boost;
+
+    protected override void OnPickup(Shark shark) {
+        Instantiate(boost, shark.transform);
+    }
+}
