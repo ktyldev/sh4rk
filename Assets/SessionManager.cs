fbd3600ae27c4ddd879085c5cef5b5563e01f443
Cat Flynn <mewtwo97@hotmail.com>
2017-08-20 15:34:06 +0100

    Start adding main menu

---
+++ Assets/SessionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SessionManager : MonoBehaviour {
+
+    public SessionManager instance { get; private set; }
+
+    public UnityEvent gameStart;
+
+    void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        gameStart = new UnityEvent();
+        SceneManager.sceneLoaded += (s, _) => {
+            if (s.name == "dev") {
+                print("Game start!");
+                gameStart.Invoke();
+            }
+        };
+    }
+}
