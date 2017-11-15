6716ba294d824710eec9e8d31ad03a3a54bcb433
Cat Flynn <mewtwo97@hotmail.com>
2017-11-15 21:12:54 +0000

    Read version from file

---
+++ Assets/SH4RK/General/Version.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Version : MonoBehaviour {
+
+    public TextAsset versionFile;
+
+    void Start() {
+        GetComponent<Text>().text = versionFile.text;
+    }
+}
