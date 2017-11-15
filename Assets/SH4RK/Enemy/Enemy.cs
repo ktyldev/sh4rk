fa7b448cd31eb874ea31027c201c778d1cd1c814
Cat Flynn <mewtwo97@hotmail.com>
2017-11-15 23:50:04 +0000

    Clean up agent controller setting code, hopefully fix enemy movement bug

--- Assets/SH4RK/Enemy/Enemy.cs
+++ Assets/SH4RK/Enemy/Enemy.cs
@@ -10,32 +10,25 @@ public class Enemy : MonoBehaviour, IAgent {
     public int level;
     public int scoreValue;
     public int engagementDistance;
-    
+
     private ExplosionManager _explosions;
     private Health _health;
 
     public UnityEvent OnDeath { get { return _health.OnEmpty; } }
 
-    public IAgentController controller {
-        get {
-            return _controller;
-        }
-    }
-    private EnemyController _controller;
+    public IAgentController controller { get; private set; }
 
     void Awake() {
         _health = GetComponent<Health>();
         _explosions = GameObject.FindGameObjectWithTag(GameTags.GameController).GetComponent<ExplosionManager>();
+        controller = Instantiate(behaviour, transform).GetComponent<IAgentController>();
     }
 
     void Start() {
-        _controller = Instantiate(behaviour, transform).GetComponent<EnemyController>();
-
         _health.OnEmpty.AddListener(Die);
     }
 
     private void Die() {
-        // onDeath.Invoke();
         Player.instance.AddScore(scoreValue);
 
         if (_explosions == null)
